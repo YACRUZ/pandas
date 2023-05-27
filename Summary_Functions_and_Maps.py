d6247f584813ade7f72133dac4f72ffdf8693f9a
YACRUZ <yamatsukino@gmail.com>
2023-05-26 22:26:17 -0600

    create summary

--- Summary_Functions_and_Maps.py
+++ Summary_Functions_and_Maps.py
@@ -26,25 +26,44 @@ print(reviews_per_country)
 
 ##Ejercicio 4
 print("===== Question 4 =====")
-#
+#Create variable centered_price containing a version of the price column with the mean price subtracted.
+centered_price = reviews.price - reviews.price.mean()
+print(centered_price)
 
 
 
 ##Ejercicio 5
 print("===== Question 5 =====")
-#
+#Create a variable bargain_wine with the title of the wine with the highest points-to-price ratio in the dataset.
+bargain_idx = (reviews.points / reviews.price).idxmax()
+bargain_wine = reviews.loc[bargain_idx, 'title']
 
 
 
 ##Ejercicio 6
 print("===== Question 6 =====")
-#
+#Create a Series descriptor_counts counting how many times each of these two words appears in the description column in the dataset
+tropy = reviews.description.map(lambda frut: "tropical" in frut).sum()
+fruits = reviews.description.map(lambda frut: "fruity" in frut).sum()
+descriptor_counts = pd.Series([tropy, fruits], index=['tropical', 'fruity'])
+print(descriptor_counts)
 
 
 
 ##Ejercicio 7
 print("===== Question 7 =====")
-#
+#Create a series star_ratings with the number of stars corresponding to each review in the dataset.
+def stars(row):
+    if row.country == 'Canada':
+        return 3
+    elif row.points >= 95:
+        return 3
+    elif row.points >= 85:
+        return 2
+    else:
+        return 1
+
+star_ratings = reviews.apply(stars, axis='columns')
 
 
 
